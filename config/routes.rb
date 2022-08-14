2d318c9c51baaace92df3d34c86c46ce2358eeac
Glen Llaci <glen_llaci@yahoo.com>
2022-08-14 22:56:40 +0200

    adding code in js.erb doc

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   resources :lesson, only: [:create, :destroy]
   resources :calendar, only: [:index, :create, :new, :show]
   resources :rooms, only: [:index, :show, :destroy]
-  resources :messages, only: [:new, :create]
+  resources :messages, only: [:new, :create, :index]
   get 'home', to: 'static_pages#home'
   get 'accueille', to: 'static_pages#accueille'
   get 'permis_auto', to: 'static_pages#permis_auto'
